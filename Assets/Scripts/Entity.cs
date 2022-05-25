9d90a95ee41f8769c54eb4d1f4d77228673d8b25
AkuroP <thomas.kang2002@gmail.com>
2022-05-25 11:08:06 +0200

    stats entity

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
+    [Header("Stat")]
+    //hp for enemy, turn left for player
+    public int maxHP;
+    private int hp;
+    //priority of entity (player always first)
+    public int prio;
+    //mobility of entity per turn
+    [Header("Movement")]
+    public int maxMobility;
+    //movement of entity in X and Y
+    public int mobilityX;
+    public int mobilityY;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        hp = maxHP;
     }
 
     // Update is called once per frame
