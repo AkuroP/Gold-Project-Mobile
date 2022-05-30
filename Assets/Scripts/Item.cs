1a24c369a3221dc02128f1986998929610715bd8
AkuroP <thomas.kang2002@gmail.com>
2022-05-30 15:38:30 +0200

    Shop Upgrade
    + item take weapon name, lvl + 1 and price

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Item : MonoBehaviour
 {
+    public Text weaponName;
+    public Text weaponUp;
+    public Text weaponUpPrice;
+
     public int itemPrice;
+    private Player player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player").GetComponent<Player>();
+        weaponName.text = player.weapon.typeOfWeapon.ToString();
+        weaponUp.text = "Level " + (player.weapon.weaponLevel + 1).ToString();
+        weaponUpPrice.text = itemPrice.ToString() + " essences";
     }
 
     // Update is called once per frame
