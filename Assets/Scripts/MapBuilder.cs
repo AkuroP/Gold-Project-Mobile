7fe29bc0a88ec6fe2ad891057621f16f66514ea6
TheMatMat <matthieu.cogez@gmail.com>
2022-06-07 13:31:26 +0200

    boss map in map builder available

--- Assets/Scripts/MapBuilder.cs
+++ Assets/Scripts/MapBuilder.cs
@@ -6,6 +6,8 @@ public class MapBuilder : MonoBehaviour
 {
     [Header("==== Map References ====")]
     public List<MapSettings> mapSettings = new List<MapSettings>();
+    public MapSettings mapSettingsBoss1 = new MapSettings();
+    public MapSettings mapSettingsBoss2 = new MapSettings();
 
     [Header("==== UI Button ====")]
     public bool enableMove;
